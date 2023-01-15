3f874642850646458883f8b0793359be8ade61ab
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-15 14:58:16 -0300

    [Ignite-React] - Add Header

--- Chapter-I/ignite-feed/src/components/Header/index.jsx
+++ Chapter-I/ignite-feed/src/components/Header/index.jsx
@@ -1,9 +1,12 @@
 import styles from "./styles.module.scss";
 
+import igniteLogo from "../../assets/ignite-logo.svg";
+
 export function Header() {
   return (
-    <div className={styles.header}>
-      <h1>Header</h1>
-    </div>
+    <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo" />
+      <span className={styles.title}>Ignite Feed</span>
+    </header>
   );
 }
