71c50a972bf3b430c3ce89b84a46e9bc6d8b44ad
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-15 15:35:47 -0300

    [Ignite-React] - Add SideBar

--- Chapter-I/ignite-feed/src/App.jsx
+++ Chapter-I/ignite-feed/src/App.jsx
@@ -1,9 +1,18 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from "./App.module.scss";
 
 export function App() {
   return (
     <div className="App">
       <Header />
+      <div className={styles.wrapper}>
+        <aside>
+          <Sidebar />
+        </aside>
+        <main>posts</main>
+      </div>
     </div>
   );
 }
