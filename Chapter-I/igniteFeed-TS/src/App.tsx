a1c147d93610e39aeaff9c2d579a87b147fb02f7
marcelosantana <marcelosantana@alu.ufc.br>
2023-01-17 19:13:57 -0300

    [Ignite-React] - Add Typescript

--- Chapter-I/igniteFeed-TS/src/App.tsx
+++ Chapter-I/igniteFeed-TS/src/App.tsx
@@ -1,6 +1,5 @@
 import { Header } from "./components/Header";
 import { Sidebar } from "./components/Sidebar";
-import { Post } from "./components/Post";
 
 import styles from "./App.module.scss";
 
